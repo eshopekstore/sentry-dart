75471a4d28172f3384d5e881a8aed5538cd549fc
eshopekstore <95987319+eshopekstore@users.noreply.github.com>
2021-12-12 13:35:25 +0000

    Revert "visibility foo"
    This reverts commit be23c31e4cbeb99ce70b05492663dd3dd662db20.

--- flutter/test/screenshot/screenshot_test.dart
+++ flutter/test/screenshot/screenshot_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:sentry_flutter/src/screenshot/sentry_screenshot.dart';
 
 void main() {
   setUp(() {
